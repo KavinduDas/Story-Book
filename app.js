b69cc670489108bf60359a0d30f46b9a76790e74
Kavindu Dassanayake <kavindud@AFSL.local>
2026-06-14 13:00:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+const port = process.env.PORT || 5000
+
+app.listen(port , ()=>{
+    console.log(`Server started at ${port}`)
+})
+
+app.get("/",(req,res) =>{
+    res.send('It Works')
+})
