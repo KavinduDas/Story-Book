49575143b7870dd663f39a802ea5877152777c2d
Kavindu Dassanayake <kavindud@AFSL.local>
2026-06-15 10:45:25 +0530

    Handle Auth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportConfig = require('./config/passport')
+
+passportConfig(passport)
 
 const app = express();
+
+
+
+//Load Routes
+const auth = require('./routes/auth')
 
 const port = process.env.PORT || 5000
 
 app.listen(port , ()=>{
     console.log(`Server started at ${port}`)
 })
+
+app.use('/auth',auth)
 
 app.get("/",(req,res) =>{
     res.send('It Works')
